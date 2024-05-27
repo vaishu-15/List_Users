3fa81262356fefd254dfc0fd02196d1e0b594e2a
vaishu-15 <vaishali15yadav@gmail.com>
2024-05-27 15:42:33 +0530

    all working good

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './src/navigation/Routes';
 import { Provider } from 'react-redux';
 import { store ,persistor} from './src/store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+
+   async function requestUserPermission() {
+     const authStatus = await messaging().requestPermission();
+     const enabled =
+       authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+       authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+     if (enabled) {
+       console.log('Authorization status:', authStatus);
+     }
+   }
+
+   const getToken = async () => {
+     const token = await messaging().getToken();
+     console.log('token=', token);
+   };
+
+   useEffect(() => {
+     requestUserPermission();
+     getToken();
+   }, []);
 
   return (
     <Provider store={store}>
